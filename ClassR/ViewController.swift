9aee9a0195d57574e919e1e31844eb11bc021352
Evan Shrestha <Evanshrestha@gmail.com>
2018-11-09 11:10:11 -0600

    Initial Commit

---
+++ ClassR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassR
+//
+//  Created by Evan Shrestha on 11/9/18.
+//  Copyright © 2018 Evan Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
