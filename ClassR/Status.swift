148154d10829111dfbe5f2aca81e890c0da25eaa
Evan Shrestha <Evanshrestha@gmail.com>
2018-11-09 12:45:11 -0600

    - Design edits

---
+++ ClassR/Status.swift
@@ -0,0 +1,13 @@
+//
+//  Status.swift
+//  ClassR
+//
+//  Created by Evan Shrestha on 11/9/18.
+//  Copyright © 2018 Evan Shrestha. All rights reserved.
+//
+
+import Foundation
+
+class Status {
+    
+}
