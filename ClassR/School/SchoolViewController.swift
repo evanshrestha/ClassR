6654aa57616f0f4066db980c227dbcfff9dfd3f0
Evan Shrestha <Evanshrestha@gmail.com>
2018-11-28 22:21:39 -0600

    - Added saved school

--- ClassR/School/SchoolViewController.swift
+++ ClassR/School/SchoolViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 import Firebase
+import CoreData
 
 class SchoolViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var schools : Dictionary<Int, School> = [:]
     var schoolIndex = 0
     
+    var savedSchoolDatabaseID = ""
+    
+    var appDelegate : AppDelegate? = UIApplication.shared.delegate as! AppDelegate
+    var context : NSManagedObjectContext?
+    var settings:NSManagedObject?
+    
     @IBOutlet weak var schoolTableView: UITableView!
     
     @IBOutlet weak var schoolTextField: UITextField!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return schools.count
     }
@@ -34,12 +42,53 @@ class SchoolViewController: UIViewController, UITableViewDelegate, UITableViewDa
         School.selectedSchool = schools[indexPath.row]
         performSegue(withIdentifier: "showSchoolPosts", sender: self)
         tableView.deselectRow(at: indexPath, animated: true)
+        do {
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Settings")
+            if let result = try? context!.fetch(request) {
+                for object in result {
+                    context!.delete(object as! NSManagedObject)
+                }
+            }
+            let entity = NSEntityDescription.entity(forEntityName: "Settings", in: context!)
+            settings = NSManagedObject(entity: entity!, insertInto: context)
+            settings!.setValue(schools[indexPath.row]!.databaseID, forKey: "school")
+            try context!.save()
+            print(settings)
+        } catch {
+            print("Failed saving")
+        }
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadSchools()
+        
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate!.persistentContainer.viewContext
+        
+        
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Settings")
+        //request.predicate = NSPredicate(format: "age = %@", "12")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let result = try context!.fetch(request)
+            
+            if (result.count == 0) {
+                let entity = NSEntityDescription.entity(forEntityName: "Settings", in: context!)
+                settings = NSManagedObject(entity: entity!, insertInto: context)
+            } else {
+            
+                for data in result as! [NSManagedObject] {
+                    settings = data
+                    savedSchoolDatabaseID = data.value(forKey: "school") as! String
+                }
+            }
+        } catch {
+            print("error with coredata")
+        }
         
     }
     
@@ -56,6 +105,13 @@ class SchoolViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 self.schools[self.schoolIndex] = school
                 
                 self.schoolIndex = self.schoolIndex + 1
+                
+                if school.databaseID == self.savedSchoolDatabaseID {
+                    School.selectedSchoolDatabaseID = school.databaseID
+                    School.selectedSchool = school
+                    self.performSegue(withIdentifier: "showSchoolPosts", sender: self)
+                }
+                
             } else {
                 print("No schools found")
             }
